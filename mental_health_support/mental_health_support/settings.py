7ff33b004f992fd5d13b93e7ccfe0732888919ab
YuvrajKhade <khadeyuvraj8@gmail.com>
2025-03-22 16:36:12 +0530

    created the schema structure:

--- mental_health_support/mental_health_support/settings.py
+++ mental_health_support/mental_health_support/settings.py
@@ -39,6 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+External_app=["chatbot","mood_tracker","resources"]
+
+INSTALLED_APPS+=External_app
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
