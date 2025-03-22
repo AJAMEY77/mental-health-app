cda71588128fd4e5efd7f30bc0a8a85d9b128a7e
YuvrajKhade <khadeyuvraj8@gmail.com>
2025-03-22 22:12:29 +0530

    added some ui related files in the respective app

--- mental_health_support/mental_health_support/settings.py
+++ mental_health_support/mental_health_support/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'rest_framework',
 ]
 
 External_app=["chatbot","mood_tracker","resources"]
@@ -58,7 +61,7 @@ ROOT_URLCONF = 'mental_health_support.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,6 +123,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "mental_health_support", "static"),
+]
+
+# Authentication settings
+LOGIN_REDIRECT_URL = 'chat'
+LOGOUT_REDIRECT_URL = 'login'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
